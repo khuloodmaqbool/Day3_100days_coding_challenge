4404a6dd0502776c6cb10850de8458c8992411b3
khuloodmaqbool <101542490+khuloodmaqbool@users.noreply.github.com>
2024-03-11 05:46:19 -0700

    Add files via upload

---
+++ Day3.ts
@@ -0,0 +1,18 @@
+ // Question 7
+ let addition:number = 5+3;
+ let subtraction:number = 10-2;
+ let multiplication:number = 4*2;
+ let division:number = 40/5;
+ 
+ console.log(` ${addition} ${subtraction} ${multiplication} ${division}`);
+ 
+           // Question 8
+ console.log(4+4);
+ console.log(16-8);
+ console.log(2*4);
+ console.log(16/2);
+ 
+          // Question 9
+ const favNum:number = 4;
+ console.log(`My favourite number is ${favNum}`);
+ 
